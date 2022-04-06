e28172ba5696d103b43962a8bed343b52fc0c204
Valeriya <v.sudarikova@internet.ru>
2022-04-06 12:16:41 +0300

    added scroll & formvalidation

--- src/js/script.js
+++ src/js/script.js
@@ -76,5 +76,62 @@ window.addEventListener('DOMContentLoaded', () => {
         plusSlide(-1)
     });
 
+    //form-validation 
 
+    const signIn = document.querySelector('#form_signIn'),
+          signUp = document.querySelector('#form_signUp');
+
+
+    function formValidate(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true, 
+                    minlength: 2
+                },
+                password: {
+                    required: true, 
+                    minlength: 8
+                },
+                check: {
+                    required: true, 
+                    minlength: 8,
+                    equalTo: '#password'
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please, enter your name",
+                    minlength: jQuery.validator.format("enter at least {2} letters")
+                }, 
+                password: {
+                    required: "Please, enter your password",
+                    minlength: jQuery.validator.format("enter at least {8} letters")
+                },
+                check: {
+                    required: "Please, enter your password again",
+                    equalTo: "The parrword doesn't match"
+                }
+            }
+        });
+    }
+
+    formValidate(signIn);
+    formValidate(signUp);
+
+    // scroll 
+
+    $(window).scroll(() => {
+        if ($(this).scrollTop() > 1000) {
+            $('.scroll-up').fadeIn();
+        } else {
+            $('.scroll-up').fadeOut();
+        }
+    });
+    $(".scroll-up").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+    
 });
